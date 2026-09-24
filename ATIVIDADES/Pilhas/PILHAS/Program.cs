2e9fa66a64d04b58c551f958ad3e4fe145064107
Gabriel Rodrigues <431629@unoesc.lan>
2026-09-23 22:00:33 -0300

    Pilha

---
+++ ATIVIDADES/Pilhas/PILHAS/Program.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections.Generic;
+
+Stack<char> chars = new Stack<char>();
+
+string text = string.Empty;
+Console.WriteLine("Digite uma palavra:");
+text = Console.ReadLine();
+
+string reversedText = string.Empty;
+foreach (char c in text) {
+    chars.Push(c);
+}
+
+while (chars.Count > 0)
+{
+        reversedText += chars.Pop();
+}
+
+Console.WriteLine(reversedText);
+
+string isPalindromo = 
+text == reversedText 
+? "É Palíndrimo" 
+: "NÃO é palíndromo";
+
+
+Console.WriteLine(isPalindromo);
+Console.WriteLine("");
